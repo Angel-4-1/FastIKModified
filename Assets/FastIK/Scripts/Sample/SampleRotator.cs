dd134953709816e2e257ee08b60ec259335fb5db
Angel-4-1 <angel.vicente01@estudiant.upf.edu>
2020-05-29 13:10:02 +0200

    changing movements

---
+++ Assets/FastIK/Scripts/Sample/SampleRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DitzelGames.FastIK
+{
+    public class SampleRotator : MonoBehaviour
+    {
+        public float speed;
+
+        void Update()
+        {
+            //just rotate the object
+            transform.Rotate(0, Time.deltaTime * speed, 0);
+        }
+    }
+}
